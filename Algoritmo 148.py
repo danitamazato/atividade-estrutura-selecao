444e755fd297a7e2e29e2f76226c701112058576
Danielle Tamazato <143049584+danitamazato@users.noreply.github.com>
2023-09-24 21:28:19 -0300

    Update Algoritmo 148.py

--- Algoritmo 148.py
+++ Algoritmo 148.py
@@ -8,7 +8,7 @@ if op == 1:
         print('\n O valor da passagem de ida-volta para R.Norte: R$950.00')
 elif op == 2:
     if iv == 1:
-        print ("\n O valor da passagem de ida-volta para R.Nordeste: R$350.00")
+        print ("\n O valor da passagem de ida para R.Nordeste: R$350.00")
     else:
         print('\n O valor da passagem de ida-volta para R.Nordeste: R$650.00')
 elif op == 3:
